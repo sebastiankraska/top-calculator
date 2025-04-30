2ae0f32ad38ad768f44b46830f8d4a1429b17499
Sebastian Kraska <140939975+sebastiankraska@users.noreply.github.com>
2025-04-30 07:23:52 +0200

    Make subtract test work, refactor jest

--- calc.js
+++ calc.js
@@ -2,4 +2,11 @@ function add(a, b) {
     return a + b;
 };
 
-module.exports = add;
+function subtract(a, b) {
+    return a - b;
+};
+
+module.exports = {
+    add,
+    subtract
+};
--- calc.spec.js
+++ calc.spec.js
@@ -1,9 +1,9 @@
-const add = require('./calc');
+const calc = require('./calc');
 
 test('adds 1 + 1 to equal 2', () => {
-    expect(add(1, 1)).toBe(2);
+    expect(calc.add(1, 1)).toBe(2);
 });
 
 test('subtracts 1 - 1 to equal 0', () => {
-    expect(subtract(1, 1).toBe(0));
+    expect(calc.subtract(1, 1)).toBe(0);
 });
