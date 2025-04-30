470dd6a81e8ba42850bd1a267b120337ab604dab
Sebastian Kraska <140939975+sebastiankraska@users.noreply.github.com>
2025-04-30 07:18:43 +0200

    Add subtract test

--- calc.spec.js
+++ calc.spec.js
@@ -3,3 +3,7 @@ const add = require('./calc');
 test('adds 1 + 1 to equal 2', () => {
     expect(add(1, 1)).toBe(2);
 });
+
+test('subtracts 1 - 1 to equal 0', () => {
+    expect(subtract(1, 1).toBe(0));
+});
