22afd4dd5199a19f5f77d50b13ca48e03a464981
Sebastian Kraska <140939975+sebastiankraska@users.noreply.github.com>
2025-04-30 07:54:16 +0200

    Add more tests (all green)

--- calc.spec.js
+++ calc.spec.js
@@ -5,16 +5,28 @@ test('adds 1 + 1 to equal 2', () => {
     expect(calc.add(1, 1)).toBe(2);
 });
 
+test('adds 21 + 3 to equal 24', () => {
+    expect(calc.add(21, 3)).toBe(24);
+});
+
+
 test('subtracts 1 - 1 to equal 0', () => {
     expect(calc.subtract(1, 1)).toBe(0);
 });
 
-
+test('subtracts 3 - 10 to equal -7', () => {
+    expect(calc.subtract(3, 10)).toBe(-7);
+});
 
 test('multiply 2 x 3 to equal 6', () => {
     expect(calc.multiply(2, 3)).toBe(6);
 });
 
+test('9 times 9 equals 81', () => {
+    expect(calc.multiply(9,9)).toBe(81);
+});
+
+// see also: https://medium.com/@afolabiwaheed/how-to-test-a-function-thats-expected-to-throw-error-in-jest-2419cc7c6462
 test('error on division by zero', () => {
     expect(() => {
         calc.divide(1,0);
