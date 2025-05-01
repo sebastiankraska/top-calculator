70ab68a18ab69a8bd6e0f1a29fe1f5c52d9e80d7
Sebastian Kraska <140939975+sebastiankraska@users.noreply.github.com>
2025-05-01 11:18:12 +0200

    Pseudo-code

--- calc.js
+++ calc.js
@@ -40,6 +40,45 @@ function operate(a,b,operator) {
     }
 };
 
+// Pseudocode 1 (Eventlistener)
+// Listen to all clicks
+// find out wich element was clicked (inside event)
+//      seperate between sorts of sources: digits vs. operators vs. equals vs. clear
+
+// Pseudocode 2 (Fortsetzung)
+//Wenn eine ZAHL gedrückt wird, 
+//     füge sie zu einer Zahlenreihe hinzu, 
+//       /z.B. wird 1 und dann 2 gedrückt, dann erstelle eine 12.)
+// Wenn ein OPERATOR gedrückt wird, 
+//     sichere / überschreibe die Operator Variable
+//      gucke, ob Input_A schon einen Wert enthält  
+//          (das spricht dafür, dass bereits eine Berechnung statt fand
+//           oder dafür, dass dies die zweite Eingabezahl ist). 
+//          Falls Input_A schon einen Wert enthält, 
+//              dann speicher die Zahlenreihe (s.o.) als Input B
+//              und starte compute() mit Input A, Input B und dem Operator 
+//              zeige das Ergebnis in HTML an
+//              speichere das Ergebnis als Input_A
+//              lösche die Operator-Variable (nötig?)
+//              lösche Input_B (wirklich nötig? Wird doch ohnehin überschrieben, oder? )
+//              (optional setze die "gedrückten" Buttons zurück (CSS Klassen))
+//          Falls nein 
+//              (falls Input A leer ist 
+//                  (das ist der Fall, 
+//                  wenn noch gar keine Berechnung lief, 
+//                  also die Seite neugeladen wurde 
+//                  oder clear gedrüclt wurde)
+//              dann speicher die zahlenreihe als Input A. 
+// Wenn RESULT gedrückt wird,
+//       prüfe ob diese Werte gesetzt wurden: Input A und Operator
+            // Wenn Ja, mache die Zahlenreihe (s.o.) zu Input B
+            // und starte die Berechnung (compute)
+            // tue hier ähnliches wie bei OPERATOR oben, sprich: mache das Ergebnis zu Input A, setze Operator-Var und InputB-Var zurück
+// Wenn CLEAR gedrückt wird,
+//       setze diese Werte zurücK: Input A, Input B, Operator
+
+// Compute sollte mehrere Dinge tun: Ergebnis anzeigen + Ergebnis als InputA abspeichern + Operator-Var und InputB-Var zurücksetzen, usw. .... so dass compute() von Klicks auf OPERATORS oder auf RESULT ausgelöst werden kann.
+
 module.exports = {
     add,
     subtract,
